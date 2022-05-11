331af2c7a14a47da785f0189a234fa570cb98de9
Iquiji <yt.failerbot.3000@gmail.com>
2022-05-11 20:52:12 +0000

    Added initial static data generation in asm. but still heavily WIP

---
+++ asm/hello_asm.asm
@@ -0,0 +1,92 @@
+struc linked_list_node
+    .next:   resd 1 ; Pointer ; if 0 there is none
+    .data:   resd 1 ; Pointer to data_ptr
+endstruc
+
+struc data_ptr
+    .type:   resd 1 ; type annotation number
+    .mem:    resd 1 ; Pointer to actual data
+endstruc
+
+struc scope_overlord
+    .parent:     resd 1 ; parent scope ; if 0 there is none
+    .scope_data: resd 1 ; pointer to scope_member list
+endstruc
+
+struc scope_member
+    .next:       resd 1 ; next scope member ; if 0 there is none
+    .identifier: resd 1 ; pointer to string corresponding to this entry
+    .data:       resd 1 ; data if the identifier is the right one
+endstruc
+
+section .data
+msg: 
+    db "Hello Assembler!", 0Ah, 0h
+fmtd_str:
+    db "%d", 0Ah, 0h
+fmtd_str_address:
+    db "address: %d", 0Ah, 0h
+    
+static0_actual db 54321 ; Integer
+static0_data_ptr_struc:
+    istruc data_ptr
+        at data_ptr.type, dd    1
+        at data_ptr.mem, dd     static0_actual
+    iend
+section .text
+
+global main
+extern printf, puts, malloc
+
+main:
+    push msg
+    call puts
+    add esp, 8
+    pop eax
+
+    mov eax, 0
+
+.inc_loop:
+    inc eax
+
+    push eax
+
+    push eax     
+    push dword fmtd_str
+    call printf
+    add esp, 8 ; realign stack pointer?
+
+    ; do stuff x times while printing ;)
+    push 1024
+    call malloc
+    add esp, 4
+
+    push eax
+    push dword fmtd_str_address
+    call printf
+    add esp, 8
+
+    ; end do stuff
+
+    pop eax
+
+    cmp eax,16
+    jne .inc_loop
+
+    ;
+
+    mov eax, static0_data_ptr_struc + data_ptr.mem
+    mov eax, [eax]
+    mov eax, [eax]
+    ; mov eax, [eax]
+    ; mov eax, [eax]
+
+    push eax     
+    push dword fmtd_str
+    call printf
+    add esp, 8 ; realign stack pointer?
+
+
+    ;;  return 0
+    mov eax, 0
+    ret
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,113 @@
-struct BackendToX86{
+use std::fs::File;
+use std::io::Write;
 
-}
-impl BackendToX86{
-    
-}
+use little_intermediate_representation::{Translator, StaticData};
+use little_parser::Parser;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Hello, world!");
+
+    let mut parser = Parser::init_with_string(r#"('5)"#);
+    let ast = parser.re_program();
+
+    let mut translator = Translator::default();
+
+    let _incomplete_res = translator.ast_to_intermediate_representation(ast);
+
+    println!("translator: {:#?}", translator);
+
+    // # Data Type Ids:
+    // 0 - Null / No Data
+    // 1 - Number -> Ptr
+    // 2 - Boolean -> Ptr
+    // 3 - Identifier -> Ptr
+    // 4 - String -> Ptr
+    // 5 - List -> Ptr to LinkedList
+    // 6 -
+    // 7 -
+    // 8 -
+    // 9 -
+
+    let mut file = File::create("out/out.asm")?;
+
+    writeln!(&mut file,"section .data")?;
+
+    for static_item in translator.static_data.iter() {
+        static_data_convert_to_handles_in_file(&mut file, static_item)?;
+    }
+
+    writeln!(&mut file,"\n\nsection .text")?;
+
+    Ok(())
+}
+
+fn static_data_convert_to_handles_in_file(file: &mut File,static_item: (&String,&StaticData)) -> Result<(),Box<dyn std::error::Error>>{
+    println!("static data item: {:#?}", static_item);
+
+    match static_item.1 {
+        little_intermediate_representation::StaticData::Bool(boolean) => {
+            writeln!(
+                file,
+                "{}_actual db {} ; Boolean",
+                static_item.0,
+                if *boolean { "0x1" } else { "0x0" }
+            )?;
+            writeln!(file, "{}_data_ptr_struc:", static_item.0,)?;
+            writeln!(
+                file,
+                "   istruc data_ptr
+    at .type, dd    2
+    at .mem, dd     {}_actual
+iend\n
+                ",
+                static_item.0
+            )?;
+        }
+        little_intermediate_representation::StaticData::Integer(integer) => {
+            writeln!(file, "{}_actual db {} ; Integer", static_item.0, integer)?;
+            writeln!(file, "{}_data_ptr_struc:", static_item.0,)?;
+            writeln!(
+                file,
+                "    istruc data_ptr
+    at .type, dd    1
+    at .mem, dd     {}_actual
+iend\n
+                ",
+                static_item.0
+            )?;
+        }
+        little_intermediate_representation::StaticData::String(string) => {
+            writeln!(file, r#"{}_actual db "{}" ; String"#, static_item.0, string)?;
+            writeln!(file, "{}_data_ptr_struc:", static_item.0,)?;
+            writeln!(
+                file,
+                "    istruc data_ptr
+    at .type, dd    4
+    at .mem, dd     {}_actual
+iend\n
+                ",
+                static_item.0
+            )?;
+        },
+        little_intermediate_representation::StaticData::Identifier(identifier) => {
+            writeln!(file, r#"{}_actual db "{}" ; Identifier"#, static_item.0, identifier)?;
+            writeln!(file, "{}_data_ptr_struc:", static_item.0,)?;
+            writeln!(
+                file,
+                "    istruc data_ptr
+    at .type, dd    3
+    at .mem, dd     {}_actual
+iend\n
+                ",
+                static_item.0
+            )?;
+        },
+        little_intermediate_representation::StaticData::List(_list) => {
+            
+            unimplemented!();
+
+        },
+    }
+
+    Ok(())
 }
